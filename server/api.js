e45d138d266b1fb3ba21d078ad114e27969af7a1
ThomasHedan <thomas.hedan56@gmail.com>
2023-03-26 19:32:55 +0200

    Update api.js

--- server/api.js
+++ server/api.js
@@ -2,6 +2,14 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 
+const {MongoClient, ObjectId} = require('mongodb');
+
+
+const MONGODB_URI = 'mongodb+srv://thomas:BoAyDxfDF5Z1r2Ha@cluster0.nuqs4kr.mongodb.net/?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'clearfashion';
+
+
+
 const PORT = 8092;
 
 const app = express();
@@ -14,8 +22,64 @@ app.use(helmet());
 
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+
+// get search (ex : http://localhost:8092/products/search?limit=10&brand=Dedicated&price=50 )
+
+async function getSearch(MONGODB_URI,brand,limit,price){
+  const client = await MongoClient.connect(MONGODB_URI, {useNewUrlParser: true});
+  const db = await client.db(MONGODB_DB_NAME);
+  
+  const collection = db.collection('products');
+  filters = {}
+
+  if(brand!=undefined){
+    filters.brand = brand;
+  }
+
+  if(price!=undefined){
+    filters.price = { $lte : parseInt(price)};
+  }
+  
+  limit = parseInt(limit);
+  
+  console.log('searching with filters = ',filters);
+  const result = await collection.find(filters).limit(limit).toArray();
+  
+  console.log(result);
+  await client.close();
+  return result;
+  
+}
+
+
+// get product by id
+
+async function getProductById(MONGODB_URI,id){
+  const client = await MongoClient.connect(MONGODB_URI, {useNewUrlParser: true});
+  const db = await client.db(MONGODB_DB_NAME);
+
+  const collection = db.collection('products');
+  const result = await collection.find({"_id":ObjectId(id)}).toArray();
+  console.log(result);
+  await client.close();
+  return result;
+  
+}
+
+app.get('/products/search', async (request, response) => {
+  console.log(request.query);
+  const brand = request.query.brand || undefined;
+  const price = request.query.price|| undefined;
+  const limit = request.query.limit || '12';
+  var res = await getSearch(MONGODB_URI,brand,limit,price);
+  response.send(res);
+});
+
+
+app.get('/products/:id', async (request, response) => {
+  const id = request.params.id;
+  var res = await getProductById(MONGODB_URI,id);
+  response.send(res);
 });
 
 app.listen(PORT);
